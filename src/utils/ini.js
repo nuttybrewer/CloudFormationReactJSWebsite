6f63cab21fc75000057b40995be2c395beb26db9
Patrick Ethier <nuttybrewer@gmail.com>
2019-08-13 00:25:38 +0200

    Finish the parser, add include hooks, handle variable interpolation, fix array mappings

--- src/utils/ini.js
+++ src/utils/ini.js
@@ -1,157 +1,228 @@
 // Modified version of ini by Isaac Z. Schlueter and Contributors
 // https://github.com/npm/ini
 
-const util = require('util');
-exports.parse = exports.decode = decode
-
-exports.stringify = exports.encode = encode
-
+// const util = require('util');
+exports.serialize = serialize;
+exports.deserialize = deserialize
 exports.safe = safe
 exports.unsafe = unsafe
 
-var eol = typeof process !== 'undefined' &&
-  process.platform === 'win32' ? '\r\n' : '\n'
 
-function encode (obj, opt) {
-  var children = []
-  var out = ''
-
+function deserialize(obj, opt) {
   if (typeof opt === 'string') {
     opt = {
-      section: opt,
       whitespace: false
     }
   } else {
     opt = opt || {}
     opt.whitespace = opt.whitespace === true
   }
+  var out = "";
 
-  var separator = opt.whitespace ? ' = ' : '='
-
-  Object.keys(obj).forEach(function (k, _, __) {
-    var val = obj[k]
-    if (val && Array.isArray(val)) {
-      val.forEach(function (item) {
-        if (opt.javapropertiesstyle) {
-          out += safe(k) + separator + safe(item) + '\n'
-        }
-        else {
-          out += safe(k + '[]') + separator + safe(item) + '\n'
-        }
-      })
-    } else if (val && typeof val === 'object') {
-      children.push(k)
-    } else {
-      out += safe(k) + separator + safe(val) + eol
-    }
-  })
-
-  if (opt.section && out.length) {
-    out = '[' + safe(opt.section) + ']' + eol + out
+  // Find the root
+  var rootLine;
+  if (obj['.'] && obj['.'].values) {
+    rootLine = obj['.'].values.find((val) => val.prevline === null);
+  }
+  if (!rootLine) {
+    return;
   }
 
-  children.forEach(function (k, _, __) {
-    var nk = dotSplit(k).join('\\.')
-    var section = (opt.section ? opt.section + '.' : '') + nk
-    var child = encode(obj[k], {
-      section: section,
-      whitespace: opt.whitespace
-    })
-    if (out.length && child.length) {
-      out += eol
-    }
-    out += child
-  })
+  var currObject = rootLine.nextline;
 
+  // Virtual vs. Real rendering
+  while(currObject) {
+    // Weird EOF condition, we don't want to add a newline if this is the last line?
+    if(!currObject.nextline){
+      out += renderValue(currObject.content, opt.virtual ? currObject.virtualvalue : currObject.value);
+    }
+    else {
+      out += renderValue(currObject.content, opt.virtual ? currObject.virtualvalue : currObject.value) + '\n';
+    }
+    currObject = currObject.nextline;
+  }
   return out
 }
 
-function dotSplit (str) {
-  return str.replace(/\1/g, '\u0002LITERAL\\1LITERAL\u0002')
-    .replace(/\\\./g, '\u0001')
-    .split(/\./).map(function (part) {
-      return part.replace(/\1/g, '\\.')
-      .replace(/\2LITERAL\\1LITERAL\2/g, '\u0001')
-    })
+function renderValue (key, val, opts) {
+  var delimiter = ',';
+  var literal = false;
+  if (opts) {
+    delimiter = opts.delimiter || delimiter;
+    literal = opts.literal || literal;
+  }
+  if(Array.isArray(val)) {
+    return key + val.join(delimiter)
+  }
+  if(literal) {
+    if(val === undefined) {
+      return key + "undefined";
+    }
+    if(val === null) {
+      return key + "null";
+    }
+  }
+  if(val) {
+    return key + val;
+  }
+  return key
 }
 
-function decode (str) {
-  var out = {}
+function serialize (str, opt) {
+  if (typeof opt === 'string') {
+    opt = {
+      delimiter: opt
+    }
+  } else {
+    opt = opt || {}
+    opt.delimiter = opt.delimiter || ','
+  }
+
+  var startingPoint = { value: null, content: null, prevline: null, nextline: null}
+  var out = {
+    '.': {
+      type: 'section',
+      values: [startingPoint]
+    }
+  }
   var p = out
+  var commentKey = p['.'].values
+  var currObject = commentKey[0]; // startingPoint above
   var section = null
+  var insideDotSection = false
+
+
   //          section     |key      = value
-  var re = /^\[([^\]]*)\]$|^([^=]+)(=(.*))?$/i
-  var lines = str.split(/[\r\n]+/g)
+  var re = /^\s*\[([^\]]*)\]$|^([^=]+)(=(.*))?$/i
+  var lines = str.split(/[\r\n]/g, -1)
 
   lines.forEach(function (line, _, __) {
-    if (!line || line.match(/^\s*[;#]/)) return
-    var match = line.match(re)
-    if (!match) return
-    if (match[1] !== undefined) {
-      section = unsafe(match[1])
-      p = out[section] = out[section] || {}
+    if (!line || line.match(/^\s*[;#]/)){
+      // Assign our value and manage the linked list
+      const val = { value: null, content: line, prevline: currObject, nextline: null}
+      currObject.nextline = val;
+      currObject = val;
+      // Push our value into the decoded object
+      commentKey.push(val);
       return
     }
+    var match = line.match(re)
+    if (!match) {
+      // Assign our value and manage the linked list
+      const val = { value: null, content: line, prevline: currObject, nextline: null}
+      currObject.nextline = val;
+      currObject = val;
+      // Push our value into the decoded object
+      commentKey.push(val);
+      return
+    }
+
+    // Match Sections in square brackets
+    if (match[1] !== undefined) {
+      section = unsafe(match[1])
+      const val = { value: null, content: line, prevline: currObject, nextline: null}
+      currObject.nextline = val;
+      currObject = val;
+      p = out[section] = out[section] || { type: 'section', values: [val], children: {}};
+      return
+    }
+    var literal = false;
     var key = unsafe(match[2])
     var value = match[3] ? unsafe(match[4]) : true
+    // Convert value to native JSON types
     switch (value) {
       case 'true':
       case 'false':
-      case 'null': value = JSON.parse(value); break;
+      case 'null':
+      case 'undefined':
+        value = JSON.parse(value);
+        literal = true
+        break;
       default:
     }
 
-    // Convert keys with '[]' suffix to an array
-    if (key.length > 2 && key.slice(-2) === '[]') {
-      key = key.substring(0, key.length - 2)
-      if (!p[key]) {
-        p[key] = []
-      } else if (!Array.isArray(p[key])) {
-        p[key] = [p[key]]
+    // Handle dot(.) sections here
+      var dotSection = key.split('.', 2);
+      if (dotSection.length > 1) {
+        p = out[dotSection[0]] = out[dotSection[0]] || { type: 'section', values: [], children: {}};
+        key = dotSection[1];
+        insideDotSection = true;
+      }
+    //
+
+    // Deal with variable substitution.
+    // Note the variables need to be "EARLIER" in the file
+    // or they won't be resolved
+    const substitutionRegex = /\${([^}]*)/gm;
+    let varMatchGroups;
+    let virtualValue = value.repeat(1);
+    while ((varMatchGroups = substitutionRegex.exec(virtualValue)) !== null) {
+      // This is necessary to avoid infinite loops with zero-width matches
+      if (varMatchGroups.index === substitutionRegex.lastIndex) {
+          substitutionRegex.lastIndex++;
+      }
+      const subKey = varMatchGroups[1];
+      const subValue = out[subKey];
+      var replaceValue;
+      if (subValue) {
+        replaceValue = subValue.values.map((obj) => {
+          if(Array.isArray(obj.value)){
+            return obj.value.join(',')
+          }
+          return obj.value
+        }).join(',');
+
+
+        if (replaceValue) {
+          const term = '${' + subKey + '}'
+          virtualValue = virtualValue.replace(term, replaceValue);
+        }
       }
     }
 
-    // Check if the key already exists, if it does, then convert it to an Array
-    if (p[key] && !Array.isArray(p[key])) {
-      p[key] = [p[key]]
+    // strip out [] in the keys
+    // We also 'split' the array here
+    // TODO: Replicate the original whitespace
+    if (key.length > 2 && key.slice(-2) === '[]') {
+      key = key.substring(0, key.length - 2)
+      value = value.split(opt.delimiter, -1);
+      virtualValue = virtualValue.split(opt.delimiter, -1);
     }
 
-    // safeguard against resetting a previously defined
-    // array by accidentally forgetting the brackets
-    if (Array.isArray(p[key])) {
-      p[key].push(value)
-    } else {
-      p[key] = value
-    }
-  })
+    // To avoid playing around with whitespace around the assignment operator
+    // Grab everything on the line until our value and keep for rendering
+    const keywithassignmentandwhitespace = /^(.*=\s*).*$/
+    const keycontent = line.match(keywithassignmentandwhitespace);
 
-  // {a:{y:1},"a.b":{x:2}} --> {a:{y:1,b:{x:2}}}
-  // use a filter to return the keys that have to be deleted.
-  Object.keys(out).filter(function (k, _, __) {
-    if (!out[k] ||
-      typeof out[k] !== 'object' ||
-      Array.isArray(out[k])) {
-      return false
-    }
-    // see if the parent section is also an object.
-    // if so, add it to that, and mark this one for deletion
-    var parts = dotSplit(k)
-    var p = out
-    var l = parts.pop()
-    var nl = l.replace(/\\\./g, '.')
-    parts.forEach(function (part, _, __) {
-      if (!p[part] || typeof p[part] !== 'object') p[part] = {}
-      p = p[part]
-    })
-    if (p === out && nl === l) {
-      return false
-    }
-    p[nl] = out[k]
-    return true
-  }).forEach(function (del, _, __) {
-    delete out[del]
-  })
+    const val = { value: value, content: keycontent[1], prevline: currObject, nextline: null, literal: literal, virtualvalue: virtualValue }
+    currObject.nextline = val;
+    currObject = val;
 
+    // Check if this is a "section"
+    if (p.children) {
+      if (p.children[key]) {
+        p.children[key].values.push(val);
+      }
+      else {
+        p.children[key] = { type: 'attribute', values: [val]}
+      }
+      if(insideDotSection) {
+        p = out;
+      }
+    }
+    else {
+      // Treat our "special" keys such as "include"
+      if (key === 'include' && opt.include_callback) {
+        opt.include_callback(out, val)
+      }
+      if (p[key]){
+        p[key].values.push(val)
+      }
+      else {
+        p[key] = { type: 'attribute', values: [val] }
+      }
+    }
+  });
   return out
 }
 
