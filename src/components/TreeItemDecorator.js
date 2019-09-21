de8c626f615037e4325694d9e770935590212c71
Patrick Ethier <nuttybrewer@gmail.com>
2019-09-21 18:26:01 +0200

    Fix root source to reflect actual path

--- src/components/TreeItemDecorator.js
+++ src/components/TreeItemDecorator.js
@@ -1,38 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {FaMinusCircle, FaPlusCircle, FaFile, FaBars} from 'react-icons/fa';
+import { FaFile, FaBars} from 'react-icons/fa';
+// import {FaMinusCircle, FaPlusCircle } from 'react-icons/fa';
 
-const Header = ({style, node}) => {
+
+const Header = ({style, customStyles, node}) => {
+
     const icon = node.extractor_key ? <FaBars/> : <FaFile/>;
     // var actions;
-    // if (node.source === '.') {
-    //   actions = <FaPlusCircle/>
+    // if (node.source === 'fieldextraction.properties.allextractors.web') {
+    //   actions = <FaPlusCircle />
     // }
     // else if (node.children) {
-    //   actions = <><FaPlusCircle /> <FaMinusCircle/></>
+    //   actions = <><FaPlusCircle /><FaMinusCircle/></>
     // }
     // else {
     //   actions = <FaMinusCircle/>
     // }
+
     // <div style={style.actions}>
     //     { actions }
     // </div>
     return (
-        <div style={style.base}>
+        <div style={style.base} >
             <div style={style.title}>
-                {icon}
+              {icon}
             </div>
             <div style={style.name}>
-                {node.name}
+              { node.name }
             </div>
-
         </div>
     );
 };
 
 Header.propTypes = {
-    node: PropTypes.object,
+    node: PropTypes.object.isRequired,
     style: PropTypes.object,
+    customStyles: PropTypes.object
+};
+
+Header.defaultProps = {
+    customStyles: {}
 };
 
 export default Header;
